2365fd1ba7de36644de5e5fe89c0d700598705b6
Darveen Vijayan <43519646+darveenvijayan@users.noreply.github.com>
2024-03-27 11:45:26 +0800

    Update batch.sh

--- batch.sh
+++ batch.sh
@@ -1,3 +1,3 @@
 #nohup python3 /content/ProductGenius/api.py &&
 python3 /content/ProductGenius/run.py &&
-streamlit run /content/ProductGenius.py
+streamlit run /content/ProductGenius/ProductGenius.py
