73216d521a0638c0eebc3691bc271dd0e0a22763
Darveen Vijayan <43519646+darveenvijayan@users.noreply.github.com>
2024-06-26 12:32:13 +0800

    Update batch.sh

--- batch.sh
+++ batch.sh
@@ -1,3 +1,3 @@
-pip install -q langchain langchain_openai chromadb tiktoken
+pip install -q langchain langchain_openai chromadb tiktoken langchain_community
 pip install -q -U fastapi uvicorn pyngrok nest_asyncio
 pip install -q -U protobuf streamlit pyngrok
